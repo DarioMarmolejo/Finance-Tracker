d37ff5115baf10f6a8594a567b9eb20b106ab17f
DarioMarmolejo <xxdausmaloxx@gmail.com>
2026-02-03 00:56:38 -0600

    Se crearon los script iniciales para la conexion de bd a n8n

---
+++ initial-data-n8n.sql
@@ -0,0 +1,11 @@
+-- 1. Crear la Categoría #1 (Para que n8n la encuentre)
+INSERT INTO categorias (id, nombre, tipo, descripcion) 
+VALUES (1, 'Alimentación', 'PERSONAL', 'Comida, despensa y restaurantes');
+
+-- 2. Crear la Cuenta #1 (Para que n8n tenga de dónde descontar)
+INSERT INTO cuentas (id, nombre, tipo, saldo_actual) 
+VALUES (1, 'Cartera Efectivo', 'EFECTIVO', 1000.00);
+
+-- (Opcional) Ajustar la secuencia para que los siguientes inserts sean 2, 3, etc.
+SELECT setval('categorias_id_seq', (SELECT MAX(id) FROM categorias));
+SELECT setval('cuentas_id_seq', (SELECT MAX(id) FROM cuentas));
